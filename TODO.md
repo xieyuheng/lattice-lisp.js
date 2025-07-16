650dbe54b1fce6aaf19f3136f00eb404ba2ec728
Xie Yuheng <xyheme@gmail.com>
2025-07-16 19:50:15 +0800

    `Type` -- `Inter` and `Union`

--- TODO.md
+++ TODO.md
@@ -1,10 +1,15 @@
-`Type` -- `Inter` and `Union`
 `Type` -- `Tau`
 `Type` -- `TauStar`
+
 `checkSubtype` -- subtyping relation of non-recursive type
+
 `parseType`
 `stmt/`
 `assert-subtype`
 `assert-not-subtype`
+
+setup test for examples
+
 `checkSubtype` -- subtyping relation of recursive type
+
 `checkEquivalent` -- test equivalent relation -- by antisymmetry of subtyping
--- src/lang/type/Type.ts
+++ src/lang/type/Type.ts
@@ -1,4 +1,4 @@
-export type Type = TypeVar | Anything | Nothing | Str | Int | Float | Arrow
+export type Type = TypeVar | Anything | Nothing | Str | Int | Float | Arrow | Inter | Union
 export type TypeVar = { kind: "TypeVar"; name: string }
 export type Anything = { kind: "Anything" }
 export type Nothing = { kind: "Nothing" }
@@ -6,3 +6,41 @@ export type Str = { kind: "Str" }
 export type Int = { kind: "Int" }
 export type Float = { kind: "Float" }
 export type Arrow = { kind: "Arrow"; argType: Type; retType: Type }
+export type Inter = { kind: "Inter"; types: Array<Type> }
+export type Union = { kind: "Union"; types: Array<Type> }
+
+export function TypeVar(name: string): TypeVar {
+  return { kind: "TypeVar", name }
+}
+
+export function Anything(): Anything {
+  return { kind: "Anything" }
+}
+
+export function Nothing(): Nothing {
+  return { kind: "Nothing" }
+}
+
+export function Str(): Str {
+  return { kind: "Str" }
+}
+
+export function Int(): Int {
+  return { kind: "Int" }
+}
+
+export function Float(): Float {
+  return { kind: "Float" }
+}
+
+export function Arrow(argType: Type, retType: Type): Arrow {
+  return { kind: "Arrow", argType, retType }
+}
+
+export function Inter(types: Array<Type>): Inter {
+  return { kind: "Inter", types }
+}
+
+export function Union(types: Array<Type>): Union {
+  return { kind: "Union", types }
+}

33799766edb287e8234ac9987f280344d79c686c
Xie Yuheng <xyheme@gmail.com>
2025-07-16 18:36:13 +0800

    setup ts project

--- TODO.md
+++ TODO.md
@@ -1,7 +1,3 @@
-setup ts project
-setup run command
-setup test scripts
-
 [type] `Type`
 [type] `assert-subtype` and `assert-not-subtype`
 [type] subtyping relation of non-recursive type
---
+++ src/commands/runCommand.ts
@@ -0,0 +1,48 @@
+import { type Command } from "@xieyuheng/commander.js"
+import fs from "fs"
+import Path from "path"
+// import { load, run } from "../lang/run/index.ts"
+
+export const runCommand: Command = {
+  name: "run",
+  description: "Run a file",
+  help(commander) {
+    return [
+      `The ${this.name} command run a file.`,
+      ``,
+      `  ${commander.name} ${this.name} <file>`,
+      ``,
+    ].join("\n")
+  },
+
+  async run(commander) {
+    const url = createURL(String(commander.args[0]))
+
+    try {
+      console.log(url)
+      // const loadedMods = new Map()
+      // const mod = await load(url, loadedMods)
+      // run(mod)
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message)
+        process.exit(1)
+      }
+
+      process.exit(1)
+    }
+  },
+}
+
+function createURL(path: string): URL {
+  try {
+    return new URL(path)
+  } catch (_) {
+    if (fs.existsSync(path) && fs.lstatSync(path).isFile()) {
+      const fullPath = Path.resolve(path)
+      return new URL(`file:${fullPath}`)
+    }
+
+    throw new Error(`I can not create URL from path: ${path}`)
+  }
+}
